8ae8d7d4427147b3e9a04ba48d9596ccbd5f75c3
Ian Edwards <ian.edward@uconn.edu>
2020-02-25 23:58:04 -0500

    update
    Homework 3 and such

---
+++ 2-25-20/07-Stu_LookMaNoHTML/Unsolved/script.js
@@ -0,0 +1,22 @@
+// Create your HTML Page via DOM Methods here!
+
+var body=document.createElement(body);
+
+// Create All Necessary Elements 
+var mainDiv=document.createElement(mainDiv);
+var imgDiv=document.createElement(imgDiv);
+var h1=document.createElement(title1);
+var h2=document.createElement(title2);
+
+//Assign Variables
+var favfoods=["Pizza","Chinese","Pasta","Sandwiches"]
+var title1=["Welcome!"]
+var title2=["Centered Text"]
+var imgDiv="."
+
+//Functions
+document.body.appendChild(title1)
+
+
+
+
---
+++ 2-25-20/09-Stu_SpeedReader/Unsolved/script.js
@@ -0,0 +1,21 @@
+var timeElem = document.query
+var mainElem 
+
+
+var poem = "Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.";
+//Scripts
+function prepareRead() {
+  // Create the countdown timer.
+  var timerCountdown = setinterval(function() {
+    secondsLeft--;
+    
+
+
+
+
+  }
+}
+
+function speedRead() {
+  // Print words to the screen one at a time.
+}
