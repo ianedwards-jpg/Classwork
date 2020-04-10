8e87fcf7ab7dfc81dd8b660efc49a8f90a36d5e3
Ian Edwards <ian.edward@uconn.edu>
2020-04-10 13:06:49 -0400

    Update Classwork Repo

---
+++ 4-7-20/01-Stu_Cats-And-Dogs/index.js
@@ -0,0 +1,19 @@
+const dog = {
+    raining: true,
+    noise: "woof",
+    makeNoise: function() {
+        if (this.raining === true) {
+            console.log(this.noise)
+        }
+    }
+}
+
+const cat = {
+    raining : false, 
+    noise: "Meow", 
+    makeNoise: function () {
+        if (this.raining === true) {
+            
+        }
+    }
+}
